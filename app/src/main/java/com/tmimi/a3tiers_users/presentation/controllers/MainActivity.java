2e1a6735dd8a5736c478854c8b9beea8af48afa2
mehdiTMIMI <mehditmimi@usmba.ac.ma>
2021-03-02 18:27:35 +0100

    updates

--- app/src/main/java/com/tmimi/a3tiers_users/presentation/controllers/MainActivity.java
+++ app/src/main/java/com/tmimi/a3tiers_users/presentation/controllers/MainActivity.java
@@ -5,22 +5,33 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.tmimi.a3tiers_users.R;
+import com.tmimi.a3tiers_users.models.User;
+import com.tmimi.a3tiers_users.utils.CustomAdapter;
+import com.tmimi.a3tiers_users.utils.CustomUserAdapter;
 import com.tmimi.a3tiers_users.utils.MyContext;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-    private RecyclerView recyclerView;
+    private ListView listView;
+    private MyContext context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView=findViewById(R.id.recycle);
-        button=findViewById(R.id.button);
-
-
+        context= (MyContext) getApplicationContext();
+        listView=findViewById(R.id.list_view);
+        CustomUserAdapter customAdapter=new CustomUserAdapter(context);
+        listView.setAdapter(customAdapter);
     }
 }
---
+++ app/src/main/java/com/tmimi/a3tiers_users/utils/CustomAdapter.java
@@ -0,0 +1,54 @@
+package com.tmimi.a3tiers_users.utils;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+import com.tmimi.a3tiers_users.R;
+import com.tmimi.a3tiers_users.models.User;
+
+import java.util.List;
+//1 - creer un custom adapter -> etendre de la classe BaseAdapter
+//2 - creation du layout : afficher 1 seul item user
+public class CustomAdapter extends BaseAdapter {
+    private List<String> userList;
+    private LayoutInflater layoutInflater;
+    public CustomAdapter(List<String> users, Context c)
+    {
+        this.userList=users;
+        layoutInflater= LayoutInflater.from(c);
+    }
+    @Override
+    public int getCount() {
+        return userList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return userList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View itemView, ViewGroup parent) {
+
+
+
+       if(itemView==null)
+        itemView=layoutInflater.inflate(R.layout.button_item,null);
+
+        Button btn1=itemView.findViewById(R.id.btn1);
+        Button btn2=itemView.findViewById(R.id.btn2);
+        btn1.setText("name : ");
+        btn2.setText(userList.get(position));
+        return itemView;
+    }
+}
